8a89be784b03254bfea85ba92e02c75c1e82027a
Semyon Pupkov <mail@semyonpupkov.com>
2022-01-25 20:30:35 +0500

    use median pivot elem

--- grokking_algo/qsort.py
+++ grokking_algo/qsort.py
@@ -2,12 +2,16 @@ def qsort(array):
     if len(array) < 2:
         return array
     else:
-        pivot = array[0]
+        median_index = len(array) // 2
+        pivot = array[median_index]
 
         less = []
         greater = []
 
-        for i in array[1:]:
+        for idx, i in enumerate(array):
+            if idx == median_index:
+                continue
+
             if i < pivot:
                 less.append(i)
             else:
@@ -16,4 +20,4 @@ def qsort(array):
         return qsort(less) + [pivot] + qsort(greater)
 
 
-print(qsort([10, 2, 30, 1, 2, 1, 5]))
+print(qsort([10, 2, 30, 20, 10, 2, 1, 5]))
