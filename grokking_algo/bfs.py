ecaf97687784b11aa43ee7a3bf5e4b31e4d69ea3
Semyon Pupkov <mail@semyonpupkov.com>
2022-02-14 19:45:16 +0500

    Add bfs

---
+++ grokking_algo/bfs.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+graph = {}
+
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["thom"] = []
+graph["jonny"] = []
+graph["peggy"] = []
+graph["anuj"] = []
+
+
+def is_seller(name):
+    return name[-1] == "m"
+
+
+def find_seller(name):
+    search_queue = deque()
+    search_queue += graph["you"]
+
+    visited = []
+
+    while search_queue:
+        person = search_queue.popleft()
+
+        if not person in visited:
+            if is_seller(person):
+                return person
+            else:
+                search_queue += graph[person]
+                visited.append(person)
+
+result = find_seller("anuj")
+
+print(result)
